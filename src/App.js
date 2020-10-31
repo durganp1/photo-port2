94cecf0f38ccea6af4e695a3cd7de98f2089d664
durganp1 <durganp1@gmail.com>
2020-10-31 13:43:16 -0500

    added Nav component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import About from './components/About';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+        <Nav></Nav>
       <main>
         <About></About>
       </main>
